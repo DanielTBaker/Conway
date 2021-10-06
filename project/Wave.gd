f22665d0aa4b3518606ec6472c0346483bc1f57b
Daniel Baker <danieltb@telus.net>
2021-10-05 21:48:07 -0400

    Tile Hit Detection

--- project/Wave.gd
+++ project/Wave.gd
@@ -1,9 +1,13 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 const MOVE_SPEED = 150
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var _contacts = []
+var _contact_tiles = []
+onready var _tilemap = get_node("../TileMap")
+
 
 var dir : Vector2 = Vector2.ZERO
 var src
@@ -15,14 +19,23 @@ func init(facing : Vector2, source):
 	dir = facing
 	src=source
 	
-func _physics_process(delta):
-	move_and_slide(dir*MOVE_SPEED)
-	var hit = false
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		print(collision.collider.name)
-		if not collision.collider == src:
-			collision.collider.hit(collision.position+dir)
-			hit = true
-	if hit:	
+#func _physics_process(delta):
+#	move_and_slide(dir*MOVE_SPEED)
+#	var hit = false
+#	for i in get_slide_count():
+#		var collision = get_slide_collision(i)
+#		print(collision.collider.name)
+#		if not collision.collider == src:
+#			collision.collider.hit(collision.position+dir)
+#			hit = true
+#	if hit:	
+#		self.queue_free()
+
+
+
+
+
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.name == "TileMap":
+		body.hit(body_shape)
 		self.queue_free()
--- project/src/Maps/Conway.gd
+++ project/src/Maps/Conway.gd
@@ -6,6 +6,7 @@ const DEATHS = [0,1,5,6,7,8]
 const BIRTHS = [3]
 const DELAY = 2
 const TILE_SIZE = 16
+const BORDER_SIZE = 3
 
 onready var timer = $Timer
 onready var players = get_node("../Players")
@@ -17,7 +18,7 @@ func _ready():
 	for x in range(WIDTH):
 		var temp = []
 		for y in range(HEIGHT):
-			temp.append(get_cell(x,y))
+			temp.append(get_cell(x+BORDER_SIZE,y+BORDER_SIZE))
 		temp_field.append(temp)
 	timer.start(DELAY)
 
@@ -30,18 +31,18 @@ func _on_Timer_timeout():
 			for x_off in [-1,0,1]:
 				for y_off in [-1,0,1]:
 					if x_off != y_off or x_off != 0:
-						if get_cell(x+x_off, y+y_off) > 0:
+						if get_cell(x+BORDER_SIZE+x_off, y+BORDER_SIZE+y_off) > 0:
 							live_neighbours +=1
-			if get_cell(x,y) == 1:
+			if get_cell(x+BORDER_SIZE,y+BORDER_SIZE) == 1:
 				if live_neighbours in DEATHS:
 					temp_field[x][y]=0
 				else:
 					temp_field[x][y]=1
-			if get_cell(x,y) == 0:
+			if get_cell(x+BORDER_SIZE,y+BORDER_SIZE) == 0:
 				if live_neighbours in BIRTHS:
 					temp_field[x][y]=1
 					for player in players.get_children():
-						if (player.get_global_position() - Vector2(TILE_SIZE*x+TILE_SIZE/2,TILE_SIZE*y+TILE_SIZE/2)).length()<TILE_SIZE:
+						if (player.get_global_position() - Vector2(TILE_SIZE*(x+BORDER_SIZE)+TILE_SIZE/2,TILE_SIZE*(y+BORDER_SIZE)+TILE_SIZE/2)).length()<TILE_SIZE:
 							temp_field[x][y]=0
 							break
 				else:
@@ -49,13 +50,18 @@ func _on_Timer_timeout():
 				
 	for x in range(WIDTH):
 		for y in range(HEIGHT):
-			set_cell(x,y,temp_field[x][y])
-	update_bitmask_region (Vector2( 0, 0 ), Vector2(WIDTH,HEIGHT))
+			set_cell(x+BORDER_SIZE,y+BORDER_SIZE,temp_field[x][y])
+	update_bitmask_region (Vector2( BORDER_SIZE, BORDER_SIZE ), Vector2(WIDTH+BORDER_SIZE,HEIGHT+BORDER_SIZE))
 	timer.start(DELAY)
 
-func hit(pos : Vector2):
-	var tile = world_to_map(pos)
-	if get_cellv(tile)==1:
-		set_cellv(tile,0)
-		temp_field[tile.x][tile.y]=0
-	update_bitmask_region (tile - Vector2(2,2), tile + Vector2(2,2))
+func hit(id : int):
+	print(id)
+	var cells = get_used_cells()
+	var cells_passable = get_used_cells_by_id(0)
+	for cell in cells_passable:
+		cells.erase(cell)
+	var pos = cells[id]
+	if get_cellv(pos)==1:
+		set_cellv(pos,0)
+		temp_field[pos.x-BORDER_SIZE][pos.y-BORDER_SIZE]=0
+	update_bitmask_region (pos - Vector2(2,2), pos + Vector2(2,2))
